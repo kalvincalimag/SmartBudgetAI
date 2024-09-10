fcc0e02dd02d1c26e3c936a7d1a59701ff95d116
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-09-10 14:37:44 +0800

    Project Initialization & Configs

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React, { useState } from "react"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import {useUser, UserButton} from '@clerk/nextjs'
+import Link from "next/link"
+
+function Header(){
+    const {user, isSignedIn} = useUser()
+
+    return (
+        <div className="p-5 flex justify-between items-center border shadow-sm z-10000">
+            <div className="flex items-center">
+                <Image src={'/advisrr.svg'} alt='logo' width={40} height={25}/>
+                <span className="text-primary font-bold text-xl">Advisrr</span>
+            </div>
+            {isSignedIn 
+                ? (<UserButton/>) 
+                : (
+                    <> 
+                        <div className="flex gap-3 items-center">
+                            <Link href='/dashboard'>
+                                <Button variant="outline" className='rounded-full'> Dashboard </Button>
+                            </Link>
+                            <Link href='/dashboard'>
+                                <Button className='rounded-full'> Get Started </Button>
+                            </Link>
+                        </div> 
+                    </>
+                )
+            }
+        </div>
+    )
+}
+
+export default Header;
