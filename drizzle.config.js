015020edeb1ec230aaf070f0cf82dc1a0f2ddc89
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-09-16 12:42:13 +0800

    Defined Schema

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,15 @@
+const dotenv = require('dotenv')
+
+dotenv.config({ path: '.env.local' })
+
+/** @type { import("drizzle-kit").Config } */
 export default {
-    dialect:'postgresql',
-    scheme:'./utils/schema.jsx',
-    out:'./drizzle',
+    schema: "./utils/schema.jsx",
+    dialect: 'postgresql',
+    out: "./drizzle",
     dbCredentials: {
-        url: process.env.NEXT_PUBLIC_DATABASE_URL,
-        connectionStrings: process.env.NEXT_PUBLIC_DATABASE_URL
+      url: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
     }
-}
+  };
+
---
+++ utils/schema.jsx
@@ -0,0 +1,31 @@
+import {
+    integer, numeric, pgTable, serial, varchar
+} from 'drizzle-orm/pg-core'
+
+// Budget Schema
+export const Budgets = pgTable('budgets', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    amount: varchar('amount', { length: 255 }).notNull(),
+    icon: varchar('icon', { length: 255 }),
+    createdBy: varchar('createdBy', { length: 255 }).notNull(),
+  });
+
+// Income Schema
+
+export const Income = pgTable('income', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    amount: varchar('amount', { length: 255 }).notNull(),
+    icon: varchar('icon', { length: 255 }),
+    createdBy: varchar('createdBy', { length: 255 }).notNull(),
+})
+
+// Expenses Schema
+export const Expenses = pgTable('expenses', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    amount: varchar('amount', { length: 255 }).notNull(),
+    budgetId: integer('budgetId').references(() => Budgets.id),
+    createdBy: varchar('createdBy', { length: 255 }).notNull(),
+  });
