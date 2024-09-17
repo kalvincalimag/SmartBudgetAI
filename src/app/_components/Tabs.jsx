eef77e8879e01dab0ec3fa426b20a99205364569
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-09-18 02:19:08 +0800

    Added tabs section to landing & updated expenses schema.

---
+++ src/app/_components/Tabs.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Image from "next/image";
+import { Tabs } from "@/components/ui/tabs";
+
+export function TabsSection() {
+  const tabs = [
+    {
+      title: "Advice",
+      value: "advice",
+      content: (
+        <div
+          className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-neutral-300 to-primary">
+          <p>Personalized Financial Advice</p>
+          <p className="text-sm"><small>Receive user-specific tailor-made AI-powered financial advice.</small></p>
+          <DummyContent src="https://res.cloudinary.com/dotera808/image/upload/v1726595676/Advisrr/Dashboard_-_Copy_pzibxk.png"/>
+        </div>
+      ),
+    },
+    {
+      title: "Analytics",
+      value: "analytics",
+      content: (
+        <div
+          className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-neutral-300 to-primary">
+          <p>Tracking & Analytics</p>
+          <p className="text-sm"><small>Track your spending and identify patterns through intuitive visualizations.</small></p>
+          <DummyContent src='https://res.cloudinary.com/dotera808/image/upload/v1726595676/Advisrr/DashboardBottom_i9oted.png' />
+        </div>
+      ),
+    },
+    {
+      title: "Budgeting",
+      value: "budgeting",
+      content: (
+        <div
+          className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-neutral-300 to-primary">
+          <p>Budget Categorization</p>
+          <p className="text-sm"><small>Allocate budgets to custom categories of your choice with ease.</small></p>
+          <DummyContent src='https://res.cloudinary.com/dotera808/image/upload/v1726594006/Advisrr/BudgetCreation_tybvid.png' />
+        </div>
+      ),
+    },
+    {
+      title: "Logging",
+      value: "logging",
+      content: (
+        <div
+          className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-neutral-300 to-primary">
+          <p>Income/Expenses Logging</p>
+          <p className="text-sm"><small>Log your income sources and daily expenses seamlessly.</small></p>
+          <DummyContent src='https://res.cloudinary.com/dotera808/image/upload/v1726595932/Advisrr/Logging_zhfacy.png'/>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    (<div
+      className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start mt-40 mb-60">
+      <Tabs tabs={tabs} />
+    </div>)
+  );
+}
+
+const DummyContent = ({src}) => {
+  return (
+    (<Image
+      src={src}
+      alt="dummy image"
+      width="2000"
+      height="2000"
+      className="object-cover object-left-top absolute mt-6 inset-x-0 w-[90%] rounded-xl mx-auto" />)
+  );
+};
--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,31 +1,30 @@
-import {
-    integer, numeric, pgTable, serial, varchar
-} from 'drizzle-orm/pg-core'
+import {integer, numeric, pgTable, serial, varchar} from 'drizzle-orm/pg-core'
 
 // Budget Schema
 export const Budgets = pgTable('budgets', {
-    id: serial('id').primaryKey(),
-    name: varchar('name', { length: 255 }).notNull(),
-    amount: varchar('amount', { length: 255 }).notNull(),
-    icon: varchar('icon', { length: 255 }),
-    createdBy: varchar('createdBy', { length: 255 }).notNull(),
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  amount: varchar('amount', { length: 255 }).notNull(),
+  icon: varchar('icon', { length: 255 }),
+  createdBy: varchar('createdBy', { length: 255 }).notNull(),
   });
 
 // Income Schema
 
 export const Income = pgTable('income', {
-    id: serial('id').primaryKey(),
-    name: varchar('name', { length: 255 }).notNull(),
-    amount: varchar('amount', { length: 255 }).notNull(),
-    icon: varchar('icon', { length: 255 }),
-    createdBy: varchar('createdBy', { length: 255 }).notNull(),
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  amount: varchar('amount', { length: 255 }).notNull(),
+  icon: varchar('icon', { length: 255 }),
+  createdBy: varchar('createdBy', { length: 255 }).notNull(),
 })
 
 // Expenses Schema
 export const Expenses = pgTable('expenses', {
-    id: serial('id').primaryKey(),
-    name: varchar('name', { length: 255 }).notNull(),
-    amount: varchar('amount', { length: 255 }).notNull(),
-    budgetId: integer('budgetId').references(() => Budgets.id),
-    createdBy: varchar('createdBy', { length: 255 }).notNull(),
-  });
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  amount: varchar('amount', { length: 255 }).notNull(),
+  budgetId: integer('budgetId').references(() => Budgets.id),
+  createdBy: varchar('createdBy', { length: 255 }).notNull(),
+  createdAt: varchar('createdAt', { length: 255 }).notNull(),
+});
