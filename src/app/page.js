3c89ff419fc845f9fed381fdf38e8c159bfad961
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-09-25 10:24:38 +0800

    Added dialog and home for UI.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
-import Image from "next/image";
+import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
+import { TabsSection } from "./_components/Tabs";
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Hero/>
+      <TabsSection/>
+      <Footer/>
     </>
   );
 }
